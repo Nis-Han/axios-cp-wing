fe5a9075f2290a50e15986e2deb39abd379b5824
Nishan <lit2019074@iiitl.ac.in>
2023-11-14 21:07:24 +0530

    Added failing test case for Login API

--- handlers/user_handler_test.go
+++ handlers/user_handler_test.go
@@ -71,7 +71,7 @@ func TestSignUp(t *testing.T) {
 	})
 }
 
-func TestLogin(t *testing.T) {
+func TestLoginwithValidCredentials(t *testing.T) {
 	loginCredentials := models.LoginData{
 		Email:    utils.GenerateRandomEmail(),
 		Password: utils.GenerateRandomPassword(),
@@ -112,4 +112,47 @@ func TestLogin(t *testing.T) {
 	}
 
 	assert.Equal(t, http.StatusOK, writer.Code)
+}
+
+func TestLoginwithInvalidCredentials(t *testing.T) {
+	loginCredentials := models.LoginData{
+		Email:    utils.GenerateRandomEmail(),
+		Password: utils.GenerateRandomPassword(),
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	MockdbInstance := mockdb.NewMockQuerier(ctrl)
+
+	MockdbInstance.EXPECT().
+		GetUser(gomock.Any(), gomock.Eq(loginCredentials.Email)).
+		Times(1).
+		DoAndReturn(func(_ any, email string) (database.User, error) {
+			salt := utils.GenerateSalt()
+			return database.User{
+				ID:             uuid.New(),
+				Email:          loginCredentials.Email,
+				FirstName:      utils.GenerateRandomName(),
+				LastName:       utils.GenerateRandomName(),
+				Salt:           salt,
+				HashedPassword: utils.HashPassword(utils.GenerateRandomPassword(), salt),
+				AuthToken:      utils.GenerateAuthToken(100),
+				IsAdminUser:    false,
+			}, nil
+		})
+
+	database.DBInstance = MockdbInstance
+
+	writer := makeRequest("POST", "/user/login", loginCredentials)
+
+	var response map[string]string
+	json.Unmarshal(writer.Body.Bytes(), &response)
+
+	error_message, exists := response["error"]
+	if exists {
+		log.Print(error_message)
+	}
+
+	assert.Equal(t, http.StatusUnauthorized, writer.Code)
 }
